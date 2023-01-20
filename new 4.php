b941eac3acbc63667c24db0afb2db56a50148013
ikayurikasari <110967668+ikayurikasari@users.noreply.github.com>
2023-01-20 19:59:02 +0700

    Add files via upload

---
+++ new 4.php
@@ -0,0 +1,7 @@
+<!-- Reprogramed By Anisyah Oktariani Supriady-->
+<?php
+	$conn = new mysqli("localhost","root","","cart_system");
+	if($conn->connect_error){
+		die("Connection Failed!".$conn->connect_error);
+	}
+?>
